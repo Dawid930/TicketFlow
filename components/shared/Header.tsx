05a3fe6a3ca86c5adc05d279a9c25c04ec18c28f
Dawid930 <davidnyikes@gmail.com>
2023-12-18 15:39:25 +0100

    implement header, navbar

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,9 +1,45 @@
-import React from "react"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 type Props = {}
 
 const Header = (props: Props) => {
-  return <div>Header</div>
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex justify-between items-center">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="TicketFlow logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 gap-3 justify-end">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild size="lg" className="rounded-full">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
