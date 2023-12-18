7cac495c6e048d98b14d83d157d983f734d00073
Dawid930 <davidnyikes@gmail.com>
2023-12-18 15:50:34 +0100

    implement footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,24 @@
-import React from "react"
+import Image from "next/image"
+import Link from "next/link"
 
 type Props = {}
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="TicketFlow logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2023 TicketFlow. All rights reserved.</p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
