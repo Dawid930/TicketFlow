ab2b58d09db0e6b51eb79355ecfc45d7a21d5c58
Dawid930 <davidnyikes@gmail.com>
2024-01-16 15:42:10 +0100

    implement Profile page Events organized part

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,67 @@
+import Collection from "@/components/shared/Collection"
+import { Button } from "@/components/ui/button"
+import { getEventsByUser } from "@/lib/actions/event.actions"
+import { auth } from "@clerk/nextjs"
+import Link from "next/link"
+import React from "react"
+
+const ProfilePage = async () => {
+  const { sessionClaims } = auth()
+
+  const userId = sessionClaims?.userId as string
+
+  const organizedEvents = await getEventsByUser({ userId, page: 1 })
+  return (
+    <>
+      {/* My tickets */}
+      <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            {/* asChild is a prop that allows us to pass a child component to the
+            button*/}
+            <Link href="/#events">Explore More Events</Link>
+          </Button>
+        </div>
+      </section>
+
+      <section className="wrapper my-8">
+        {/* <Collection
+          data={events?.data}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries  - you can always find great events here"
+          collectionType="My_tickets"
+          limit={3}
+          page={1}
+          urlParamName="ordersPage"
+          totalPages={2}
+        /> */}
+      </section>
+      {/* Events organized */}
+      <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left">Events organized</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            {/* asChild is a prop that allows us to pass a child component to the
+            button*/}
+            <Link href="/events/create">Create new event</Link>
+          </Button>
+        </div>
+      </section>
+      <section className="wrapper my-8">
+        <Collection
+          data={organizedEvents?.data}
+          emptyTitle="No event tickets have been created yet"
+          emptyStateSubtext="Go, create some now!"
+          collectionType="Events_Organized"
+          limit={6}
+          page={1}
+          urlParamName="eventsPage"
+          totalPages={2}
+        />
+      </section>
+    </>
+  )
+}
+
+export default ProfilePage
