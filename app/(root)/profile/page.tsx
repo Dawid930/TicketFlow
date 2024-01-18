ae6ae5f92fcf3d43100f190f03727e1235135a3b
Dawid930 <davidnyikes@gmail.com>
2024-01-18 16:51:42 +0100

    add search, category filter, pagination

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,16 +1,26 @@
 import Collection from "@/components/shared/Collection"
 import { Button } from "@/components/ui/button"
 import { getEventsByUser } from "@/lib/actions/event.actions"
+import { getOrdersByUser } from "@/lib/actions/order.actions"
+import { IOrder } from "@/lib/database/models/order.model"
+import { SearchParamProps } from "@/types"
 import { auth } from "@clerk/nextjs"
 import Link from "next/link"
 import React from "react"
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth()
 
   const userId = sessionClaims?.userId as string
 
-  const organizedEvents = await getEventsByUser({ userId, page: 1 })
+  const ordersPage = Number(searchParams?.ordersPage) || 1
+  const eventsPage = Number(searchParams?.eventsPage) || 1
+
+  const orders = await getOrdersByUser({ userId, page: 1 })
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || []
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
+
   return (
     <>
       {/* My tickets */}
@@ -26,24 +36,22 @@ const ProfilePage = async () => {
       </section>
 
       <section className="wrapper my-8">
-        {/* <Collection
-          data={events?.data}
+        <Collection
+          data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries  - you can always find great events here"
           collectionType="My_tickets"
           limit={3}
-          page={1}
+          page={ordersPage}
           urlParamName="ordersPage"
-          totalPages={2}
-        /> */}
+          totalPages={orders?.totalPages}
+        />
       </section>
       {/* Events organized */}
       <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events organized</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
-            {/* asChild is a prop that allows us to pass a child component to the
-            button*/}
             <Link href="/events/create">Create new event</Link>
           </Button>
         </div>
@@ -55,9 +63,9 @@ const ProfilePage = async () => {
           emptyStateSubtext="Go, create some now!"
           collectionType="Events_Organized"
           limit={6}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -39,7 +39,7 @@ type EventFormProps = {
 
 const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([])
-  const [startDate, setStartDate] = useState(new Date())
+
   const initialValues =
     event && type === "Update"
       ? {
