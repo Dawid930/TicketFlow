e186bdaf070621bd83ed321dddc75bb27a04cef4
Dawid930 <davidnyikes@gmail.com>
2024-01-21 15:17:39 +0100

    fix category name

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -46,7 +46,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
               {event.isFree ? "FREE" : `${event.price}`}
             </span>
             <p className="p-semibold-14 rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
-              {event.category.name}
+              {event.category?.name}
             </p>
           </div>
         )}
